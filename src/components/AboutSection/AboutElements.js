fab06c724cd1bd639b2836772fffb722ef17b571
Shiv Patel <shiv.p.patel@outllok.com>
2020-11-30 04:56:11 -0500

    made about me page responsive

--- src/components/AboutSection/AboutElements.js
+++ src/components/AboutSection/AboutElements.js
@@ -3,10 +3,11 @@ import styled from "styled-components";
 export const AboutContainer = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
     padding: 0 30px;
     height: 100vh;
+    width: 100vw;
     position: relative;
     z-index: 1;
     background: #DAE0E2;
@@ -16,15 +17,31 @@ export const AboutWrapper = styled.div`
     flex-wrap:wrap;
     justify-content: space-evenly;
     align-items: center;
+    height: 50vh;
+    @media screen and (max-width: 768px){
+        height: 70vh;
+    }
 `;
 
 export const AboutContent = styled.div`
     display: flex;
     flex-direction:column;
-    padding-right:50px;
+    margin-right:20px;
+    margin-left: 20px;
+    padding:20px;
+    background: white;
+    border-radius: 10px;
+    max-width: 90vw;
+    box-shadow: -5px 6px #A4B0BD;
 `;
 export const ContentItem = styled.div`
     padding: 15px;
+    margin-left: auto;
+    margin-right:auto;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    
     @media screen and (max-width: 768px){
         padding: 18px;
     }
@@ -33,20 +50,18 @@ export const ContentItem = styled.div`
     }
 `;
 export const AboutTitle = styled.h1`
-    margin-top: 140px;
     margin-bottom: 40px;
     color: #0A79DF;
-    font-size: 30px;
+    font-size: 32px;
     max-width: 480px;
     text-align:center;
 
     @media screen and (max-width: 768px){
-        font-size: 26px;
-        margin-top: 150px;
+        font-size: 28px;
         margin-bottom: 20px;
     }
     @media screen and (max-width: 480px){
-        font-size: 22px;
+        font-size: 24px;
     }
 `;
 export const ContentTitle = styled.h2`
@@ -77,6 +92,7 @@ export const ContentBody = styled.p`
     }
 `;
 export const AboutImage = styled.div`
+    padding: 30px;
     
 `;
 export const Image = styled.img`
