30cdde4b7a3b31ef2ef845287a5f7b75eaae6aab
shivppatel <shiv.p.patel@outlook.com>
2020-11-30 09:15:52 -0500

    setup data retrieval for heatmap

--- src/components/MapSection/index.js
+++ src/components/MapSection/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import {MapContainer, MapWrapper, MapContent, ContentTitle} from './MapElements'
-
+fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json').then(response => response.json())
+.then(data => {
+    data.forEach(element => {
+        crimePoints.push([element['location']['latitude'], element['location']['longitude']]);
+    });
+})
+const crimePoints = [];
+console.log(crimePoints);
 const Map = () => {
     return (
         <MapContainer id="map">
