b243b0b68bfcd0ad67ec2034155255dfca1c435a
connorowen7 <7connor28@comcast.net>
2020-12-01 20:12:33 -0500

    Update index.js

--- src/components/DocumentationSection/index.js
+++ src/components/DocumentationSection/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ContentItem, ContentTitle } from '../AboutSection/AboutElements'
 import {DocContainer, DocTitle, DocWrapper, DocContent} from './DocumentationElements'
 
 const Documentation = () => {
@@ -6,9 +7,30 @@ const Documentation = () => {
         <DocContainer id="documentation">
             <DocTitle>Documentation</DocTitle>
             <DocWrapper>
-                <DocContent>
-                    <h1>Documentation will go here</h1>
-                </DocContent>
+                <IntroductionContent>
+                    <ContentItem>
+                        <ContentTitle>Introduction</ContentTitle>
+                        <h1>Documentation will go here</h1>
+                    </ContentItem>
+                </IntroductionContent>
+
+                <AccountContent>
+                    <ContentItem>
+                        <ContentTitle>Account Setup</ContentTitle>
+                    </ContentItem>
+                </AccountContent>
+
+                <VisualContent>
+                    <ContentItem>
+                        <ContentTitle>Advanced Visualations</ContentTitle>
+                    </ContentItem>
+                </VisualContent>
+
+                <VisualExportContent>
+                    <ContentItem>
+                        <ContentTitle>Exporting Visualations</ContentTitle>
+                    </ContentItem>
+                </VisualExportContent>
             </DocWrapper>
         </DocContainer>
     )
