fc575dccebaf6913bc5c8a449ff53d25f4e9c79e
Shiv Patel <shiv.p.patel@outlook.com>
2020-12-03 10:57:38 -0500

    updated navbar background on scroll

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -5,7 +5,7 @@ import { Link as LinkS } from "react-scroll";
 
 
 export const Nav = styled.nav`
-  background: #0A79DF;
+  background: ${({ scrollNav}) => (scrollNav ? '#0A79DF' : 'transparent')};
   height: 80px;
   margin-top: -80px;
   display: flex;
