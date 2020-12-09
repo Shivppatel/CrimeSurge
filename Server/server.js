63e1da376c3cf9fb43df5de2f29c3739262ea601
Shiv Patel <shiv.p.patel@outlook.com>
2020-12-09 18:05:05 -0500

    setup backend and fontend to start concurrently

---
+++ Server/server.js
@@ -0,0 +1,47 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const express = require('express');
+const dotenv = require('dotenv');
+const fetch = require('node-fetch');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('../Client/build'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.route('/api')
+  .get((req, res) => {
+    console.log('GET request detected');
+    res.send('Api get request');
+    /* const data = await fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json');
+    const json = await data.json();
+    console.log('data from fetch', json); */
+  })
+  /* Callback for lab 7 */
+  .post(async (req, res) => {
+    /*  Await statement for crimes */
+    /* API for PG County Crimes */
+    const nuData = await fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json');
+    const nuJson2 = await nuData.json();
+    res.json(nuJson2);
+    console.log('POST request detected');
+    /* res.status(200).send('Hello World'); */
+    /* console.log('Form data in res.body', req.body); */
+    /* Log statement for crimes */
+    console.log('fetch request data', nuJson2);
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
