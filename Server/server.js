89fee1e4afa5a189e84f675bfc5ae0390a9b7890
Shiv Patel <shiv.p.patel@outlook.com>
2020-12-09 18:20:05 -0500

    hopefully fixed error with heroku deployment

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const fetch = require('node-fetch');
+const path = require('path');
 
 dotenv.config();
 
@@ -12,14 +13,16 @@ const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('../Client/build'));
+app.use(express.static(path.join(__dirname, '../Client/build')));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Client/build', 'index.html'))
+})
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
