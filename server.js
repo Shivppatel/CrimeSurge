23777829c473e11f14a01a5352e0dd824800dcdd
Shiv Patel <shiv.p.patel@outlook.com>
2020-12-09 20:07:23 -0500

    Setup connection to MongoDB using mongoose and set an endpoint for /api

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 5000;
+app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+
+const CONNECTION_URL =
+  "mongodb+srv://shivppatel:pasword123@cluster0.dinmg.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+  mongoose.set('useFindAndModify', false);
+
 // API calls
+app.get('/api', (req, res) => {
+  res.send('Hello welcome to CrimeSurge\'s API')
+})
 app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Express" });
 });
@@ -22,5 +41,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "Client/build", "index.html"));
   });
+  app.get("/home", function (req, res) {
+    res.sendFile(path.join(__dirname, "Client/build", "index.html"));
+  });
 }
-app.listen(port, () => console.log(`Listening on port ${port}`));
