8205b365d368d54ebcb50abe1c3666394ef95b60
shivppatel <shiv.p.patel@outlook.com>
2020-12-12 04:07:20 -0500

    updated look of map section and also removed test api endpoint

--- Client/src/components/MapSection/MapElements.js
+++ Client/src/components/MapSection/MapElements.js
@@ -4,20 +4,34 @@ export const MapContainer = styled.div`
   background: #a3c2f0;
   justify-content: center;
   align-items: center;
-  height: 95vh;
+  height: 92vh;
   position: relative;
   z-index: 1;
-  padding:30px;
 `;
 
-export const ContentTitle = styled.h1`
-  margin-bottom: 10px;
+export const MapWrapper = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content:center;
+align-items: center;
+width: 98vw;
+height: 85vh;
+`;
+
+export const MapTitle = styled.h1`
+  padding-top: 20px;
   font-size: 2.5rem;
   text-align: center;
-  color: white;
-  padding: 30px;
+  color: #17263c;
 
   @media screen and (max-width: 480px) {
     font-size: 2rem;
   }
+`;
+
+export const IncidentMap = styled.div`
+flex:0.8;
+height: 95%;
+min-width:300px;
+box-shadow: 0px 0px 5px #17263c;
 `;
--- Client/src/components/MapSection/index.js
+++ Client/src/components/MapSection/index.js
@@ -1,15 +1,31 @@
 import React from "react";
-import { MapContainer, ContentTitle } from "./MapElements";
+import {
+  MapContainer,
+  IncidentMap,
+  MapWrapper,
+  MapTitle,
+} from "./MapElements";
 import GoogleMapReact from "google-map-react";
 
-import MarkerImage from '../../images/marker.png'
+import MarkerImage from "../../images/marker.png";
 class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: null,
       isLoaded: false,
+      formData: {
+        type: null,
+        date: null,
+        location: null,
+      },
     };
+  }
+
+  setDate(){
+    this.setState({
+        
+    })
   }
 
   async componentDidMount() {
@@ -26,163 +42,172 @@ class Map extends React.Component {
       }
     }
   }
-  
+
   render() {
     const crimeList = this.state.data;
-
+    const formData = this.state.formData;
+    console.log(formData['date']);
     return (
       <MapContainer id="map">
-        {this.state.isLoaded ? (
-          <GoogleMapReact
-            bootstrapURLKeys={{
-              key: "AIzaSyAZ8RBpp3vhVQbAv_WZTWu-FQhH8EOTtTM",
-              libraries: ["visualization"],
-            }}
-            defaultCenter={{ lat: 38.98, lng: -76.94 }}
-            defaultZoom={12}
-            scaleControl={true}
-            yesIWantToUseGoogleMapApiInternals
-            options={{
-              minZoom: 11,
-              maxZoom: 18,
-              styles: [
-                { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-                {
-                  elementType: "labels.text.stroke",
-                  stylers: [{ color: "#242f3e" }],
-                },
-                {
-                  elementType: "labels.text.fill",
-                  stylers: [{ color: "#746855" }],
-                },
-                {
-                  featureType: "administrative.locality",
-                  elementType: "labels.text.fill",
-                  stylers: [{ color: "#d59563" }],
-                },
-                {
-                  featureType: "poi",
-                  elementType: "labels.text.fill",
-                  stylers: [{ color: "#d59563" }],
-                },
-                {
-                  featureType: "poi.park",
-                  elementType: "geometry",
-                  stylers: [{ color: "#263c3f" }],
-                },
-                {
-                  featureType: "poi.park",
-                  elementType: "labels.text.fill",
-                  stylers: [{ color: "#6b9a76" }],
-                },
-                {
-                  featureType: "road",
-                  elementType: "geometry",
-                  stylers: [{ color: "#38414e" }],
-                },
-                {
-                  featureType: "road",
-                  elementType: "geometry.stroke",
-                  stylers: [{ color: "#212a37" }],
-                },
-                {
-                  featureType: "road",
-                  elementType: "labels.text.fill",
-                  stylers: [{ color: "#9ca5b3" }],
-                },
-                {
-                  featureType: "road.highway",
-                  elementType: "geometry",
-                  stylers: [{ color: "#746855" }],
-                },
-                {
-                  featureType: "road.highway",
-                  elementType: "geometry.stroke",
-                  stylers: [{ color: "#1f2835" }],
-                },
-                {
-                  featureType: "road.highway",
-                  elementType: "labels.text.fill",
-                  stylers: [{ color: "#f3d19c" }],
-                },
-                {
-                  featureType: "transit",
-                  elementType: "geometry",
-                  stylers: [{ color: "#2f3948" }],
-                },
-                {
-                  featureType: "transit.station",
-                  elementType: "labels.text.fill",
-                  stylers: [{ color: "#d59563" }],
-                },
-                {
-                  featureType: "water",
-                  elementType: "geometry",
-                  stylers: [{ color: "#17263c" }],
-                },
-                {
-                  featureType: "water",
-                  elementType: "labels.text.fill",
-                  stylers: [{ color: "#515c6d" }],
-                },
-                {
-                  featureType: "water",
-                  elementType: "labels.text.stroke",
-                  stylers: [{ color: "#17263c" }],
-                },
-              ],
-            }}
-            onGoogleApiLoaded={({ map, maps }) => {
-              const heatmap = new maps.visualization.HeatmapLayer({
-                data: crimeList?.map((point) => ({
-                  location: new maps.LatLng(
-                    point["location"]["lat"],
-                    point["location"]["lng"]
-                  ),
-                  weight: 3,
-                })),
-                radius: 15,
-              });
-              heatmap.setMap(map);
-              crimeList?.forEach((point) => {
-                var type = point["type"];
-                var date = new Date(point["date"]).toLocaleDateString();
-                var contentString =
-                  '<div id="content">' +
-                  '<div id="siteNotice">' +
-                  "</div>" +
-                  '<h2 id="firstHeading" class="firstHeading">' +
-                  type +
-                  "</h2>" +
-                  '<div id="bodyContent">' +
-                  "<p><b>" +
-                  date +
-                  "</b></p>" +
-                  "</div>" +
-                  "</div>";
-                const infowindow = new maps.InfoWindow({
-                  content: contentString,
-                });
-                const marker = new maps.Marker({
-                  position: new maps.LatLng(
-                    point["location"]["lat"],
-                    point["location"]["lng"]
-                  ),
-                  map,
-                  title: point["type"],
-                  icon: MarkerImage
-                });
-                marker.addListener("click", () => {
-                  infowindow.open(map, marker);
-                });
-              });
-            }}
-          ></GoogleMapReact>
-        ) : (
-            <ContentTitle>
-              Sorry we had a problem retrieving the crime data...
-            </ContentTitle>
-        )}
+        <MapTitle>Crime Map</MapTitle>
+        <MapWrapper>
+          <IncidentMap>
+            {this.state.isLoaded ? (
+              <GoogleMapReact
+                bootstrapURLKeys={{
+                  key: "AIzaSyAZ8RBpp3vhVQbAv_WZTWu-FQhH8EOTtTM",
+                  libraries: ["visualization"],
+                }}
+                defaultCenter={{ lat: 38.98, lng: -76.94 }}
+                defaultZoom={12}
+                scaleControl={true}
+                yesIWantToUseGoogleMapApiInternals
+                options={{
+                  minZoom: 11,
+                  maxZoom: 18,
+                  styles: [
+                    {
+                      elementType: "geometry",
+                      stylers: [{ color: "#242f3e" }],
+                    },
+                    {
+                      elementType: "labels.text.stroke",
+                      stylers: [{ color: "#242f3e" }],
+                    },
+                    {
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#746855" }],
+                    },
+                    {
+                      featureType: "administrative.locality",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#d59563" }],
+                    },
+                    {
+                      featureType: "poi",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#d59563" }],
+                    },
+                    {
+                      featureType: "poi.park",
+                      elementType: "geometry",
+                      stylers: [{ color: "#263c3f" }],
+                    },
+                    {
+                      featureType: "poi.park",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#6b9a76" }],
+                    },
+                    {
+                      featureType: "road",
+                      elementType: "geometry",
+                      stylers: [{ color: "#38414e" }],
+                    },
+                    {
+                      featureType: "road",
+                      elementType: "geometry.stroke",
+                      stylers: [{ color: "#212a37" }],
+                    },
+                    {
+                      featureType: "road",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#9ca5b3" }],
+                    },
+                    {
+                      featureType: "road.highway",
+                      elementType: "geometry",
+                      stylers: [{ color: "#746855" }],
+                    },
+                    {
+                      featureType: "road.highway",
+                      elementType: "geometry.stroke",
+                      stylers: [{ color: "#1f2835" }],
+                    },
+                    {
+                      featureType: "road.highway",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#f3d19c" }],
+                    },
+                    {
+                      featureType: "transit",
+                      elementType: "geometry",
+                      stylers: [{ color: "#2f3948" }],
+                    },
+                    {
+                      featureType: "transit.station",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#d59563" }],
+                    },
+                    {
+                      featureType: "water",
+                      elementType: "geometry",
+                      stylers: [{ color: "#17263c" }],
+                    },
+                    {
+                      featureType: "water",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#515c6d" }],
+                    },
+                    {
+                      featureType: "water",
+                      elementType: "labels.text.stroke",
+                      stylers: [{ color: "#17263c" }],
+                    },
+                  ],
+                }}
+                onGoogleApiLoaded={({ map, maps }) => {
+                  const heatmap = new maps.visualization.HeatmapLayer({
+                    data: crimeList?.map((point) => ({
+                      location: new maps.LatLng(
+                        point["location"]["lat"],
+                        point["location"]["lng"]
+                      ),
+                      weight: 3,
+                    })),
+                    radius: 15,
+                  });
+                  heatmap.setMap(map);
+                  crimeList?.forEach((point) => {
+                    var type = point["type"];
+                    var date = new Date(point["date"]).toLocaleDateString();
+                    var contentString =
+                      '<div id="content">' +
+                      '<div id="siteNotice">' +
+                      "</div>" +
+                      '<h2 id="firstHeading" class="firstHeading">' +
+                      type +
+                      "</h2>" +
+                      '<div id="bodyContent">' +
+                      "<p><b>" +
+                      date +
+                      "</b></p>" +
+                      "</div>" +
+                      "</div>";
+                    const infowindow = new maps.InfoWindow({
+                      content: contentString,
+                    });
+                    const marker = new maps.Marker({
+                      position: new maps.LatLng(
+                        point["location"]["lat"],
+                        point["location"]["lng"]
+                      ),
+                      map,
+                      title: point["type"],
+                      icon: MarkerImage,
+                    });
+                    marker.addListener("click", () => {
+                      infowindow.open(map, marker);
+                    });
+                  });
+                }}
+              ></GoogleMapReact>
+            ) : (
+              <MapTitle>
+                Sorry we had a problem retrieving the crime data...
+              </MapTitle>
+            )}
+          </IncidentMap>
+        </MapWrapper>
       </MapContainer>
     );
   }
